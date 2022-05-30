c410a39a771aeafd66c8440efde711552425154d
Haoming <73768377+Haoming02@users.noreply.github.com>
2022-05-30 22:14:33 +0800

    Ver 2.01
    Fixed Bugs

--- functions.py
+++ functions.py
@@ -6,17 +6,17 @@ def replaceID(file, oldID, newID):
     l = os.path.getsize(file)
 
     with open(file, 'r+b') as FILE:
-        for i in range(l - ID_LENGTH):
+        for i in range(l - common.ID_LENGTH):
             FILE.seek(i)
-            byte = FILE.read(ID_LENGTH)
+            byte = FILE.read(common.ID_LENGTH)
 
             if byte == str.encode(oldID):
                 FILE.seek(i)
                 FILE.write(str.encode(newID))
 
-        for i in range(l - ID_SHORT):
+        for i in range(l - common.ID_SHORT):
             FILE.seek(i)
-            byte = FILE.read(ID_SHORT)
+            byte = FILE.read(common.ID_SHORT)
 
             if byte == str.encode(oldID[0:4]):
                 FILE.seek(i)
@@ -28,7 +28,7 @@ def getHash(ID):
     prefab = f'3d/chara/body/bdy{ID}/pfb_bdy{ID}'
     material = f'sourceresources/3d/chara/body/bdy{ID}/materials/mtl_bdy{ID}'
 
-    pfb = pd.read_sql_query(f'select * from a where "n" = "{prefab}";', META)
-    mat = pd.read_sql_query(f'select * from a where "n" = "{material}";', META)
+    pfb = pd.read_sql_query(f'select * from a where "n" = "{prefab}";', common.META)
+    mat = pd.read_sql_query(f'select * from a where "n" = "{material}";', common.META)
 
     return pfb.iloc[0,6], mat.iloc[0,6]
